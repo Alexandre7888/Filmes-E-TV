711ccd5a883655176def09bd1f2c7854f0a77d79
Alexandre7888 <braatzjunioralexandre5@gmail.com>
2025-07-22 14:53:05 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 const CACHE_NAME = "itv-cache-v1";
 const urlsToCache = [
-  "ver.html?token=PUBLICO",
+  "ver.html",
   "manifest.json",
   "https://alexandre7888.github.io/Filmes-E-TV/file_00000000629c61f5ab79dacd3f1866e9.png"
 ];
