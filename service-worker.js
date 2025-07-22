e467536a8d0f40c60d1d740c52655c970c83ad79
Alexandre7888 <braatzjunioralexandre5@gmail.com>
2025-07-22 14:22:47 -0300

    Criar service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = "itv-cache-v1";
+const urlsToCache = [
+  "ver-privado.html?token=PUBLICO",
+  "manifest.json",
+  "https://alexandre7888.github.io/Filmes-E-TV/file_00000000629c61f5ab79dacd3f1866e9.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
